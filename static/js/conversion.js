a4ed1549487dc195f1ad1d4ffbcedf6b8e76e254
NSKVamsi Devarapalli <naga.devarapalli.18@mountblue.tech>
2022-01-20 11:42:19 +0530

    updated coversion.js

--- static/js/conversion.js
+++ static/js/conversion.js
@@ -17,22 +17,22 @@ function convert() {
                     element.innerHTML = element.getAttribute("value")
                 }
             }
-        }).catch(reject => {
-            console.log(reject)
-            if (document.getElementById("select-value").value === "USD") {
-                for (var element of document.getElementsByClassName("change")) {
-                    value = Number(element.getAttribute("value")) * 1.14
-                    element.innerHTML = (Math.ceil(value * 20) / 20).toFixed(2)
-                }
-            } else if (document.getElementById("select-value").value === "INR") {
-                for (var element of document.getElementsByClassName("change")) {
-                    value = Number(element.getAttribute("value")) * 84.81
-                    element.innerHTML = (Math.ceil(value * 20) / 20).toFixed(2)
-                }
-            } else if (document.getElementById("select-value").value === "EUR") {
-                for (var element of document.getElementsByClassName("change")) {
-                    element.innerHTML = element.getAttribute("value")
-                }
-            }
+            // }).catch(reject => {
+            //     console.log(reject)
+            //     if (document.getElementById("select-value").value === "USD") {
+            //         for (var element of document.getElementsByClassName("change")) {
+            //             value = Number(element.getAttribute("value")) * 1.14
+            //             element.innerHTML = (Math.ceil(value * 20) / 20).toFixed(2)
+            //         }
+            //     } else if (document.getElementById("select-value").value === "INR") {
+            //         for (var element of document.getElementsByClassName("change")) {
+            //             value = Number(element.getAttribute("value")) * 84.81
+            //             element.innerHTML = (Math.ceil(value * 20) / 20).toFixed(2)
+            //         }
+            //     } else if (document.getElementById("select-value").value === "EUR") {
+            //         for (var element of document.getElementsByClassName("change")) {
+            //             element.innerHTML = element.getAttribute("value")
+            //         }
+            //     }
         });
 }
